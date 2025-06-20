5412cb0a668040032924d3c5f96406c36f97b3e4
Nikita Salikov <nik5275622@mail.ru>
2025-06-20 13:30:02 +0300

    impoved single decorator logic

--- src/distributed_lock/distributed_lock.py
+++ src/distributed_lock/distributed_lock.py
@@ -1,9 +1,18 @@
 import datetime
 import functools
+import uuid
 
 import redis
 
 redis_client = redis.StrictRedis(host="127.0.0.1", port=6379, db=0)
+
+RELEASE_LUA = """
+if redis.call('get', KEYS[1]) == ARGV[1] then
+    return redis.call('del', KEYS[1])
+else
+    return 0
+end
+"""
 
 
 def single(max_processing_time: datetime.timedelta):
@@ -12,8 +21,9 @@ def single(max_processing_time: datetime.timedelta):
         def wrapper(*args, **kwargs):
             lock_key = f"single_lock:{func.__module__}.{func.__name__}"
             lock_ttl = int(max_processing_time.total_seconds())
+            token = str(uuid.uuid4())
             acquired = redis_client.set(
-                lock_key, "locked", nx=True, ex=lock_ttl
+                lock_key, token, nx=True, ex=lock_ttl
             )
             if not acquired:
                 raise RuntimeError(
@@ -22,7 +32,7 @@ def single(max_processing_time: datetime.timedelta):
             try:
                 return func(*args, **kwargs)
             finally:
-                redis_client.delete(lock_key)
+                redis_client.delete(RELEASE_LUA, 1, lock_key, token)
 
         return wrapper
 
